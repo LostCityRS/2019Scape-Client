05e295dff380ed24da0991a423e04d74c38a8fb6
Pazaz <pazaz@itspazaz.com>
2024-10-30 18:04:01 -0400

    fix: JVM vendor-based error

--- client/src/main/java/com/jagex/game/client/BrowserControl.java
+++ client/src/main/java/com/jagex/game/client/BrowserControl.java
@@ -2,7 +2,7 @@ package com.jagex.game.client;
 
 import deob.ObfuscatedName;
 import java.applet.Applet;
-import netscape.javascript.JSObject;
+// import netscape.javascript.JSObject;
 
 @ObfuscatedName("act")
 public class BrowserControl {
@@ -13,16 +13,16 @@ public class BrowserControl {
 
 	@ObfuscatedName("act.e(Ljava/applet/Applet;Ljava/lang/String;B)V")
 	public static void eval(Applet arg0, String arg1) throws Throwable {
-		JSObject.getWindow(arg0).eval(arg1);
+		// JSObject.getWindow(arg0).eval(arg1);
 	}
 
 	@ObfuscatedName("act.n(Ljava/applet/Applet;Ljava/lang/String;I)Ljava/lang/Object;")
 	public static Object call(Applet arg0, String arg1) throws Throwable {
-		return JSObject.getWindow(arg0).call(arg1, null);
+		return null; // JSObject.getWindow(arg0).call(arg1, null);
 	}
 
 	@ObfuscatedName("act.m(Ljava/applet/Applet;Ljava/lang/String;[Ljava/lang/Object;I)Ljava/lang/Object;")
 	public static Object call(Applet arg0, String arg1, Object[] arg2) throws Throwable {
-		return JSObject.getWindow(arg0).call(arg1, arg2);
+		return null; // JSObject.getWindow(arg0).call(arg1, arg2);
 	}
 }
